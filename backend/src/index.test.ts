361bd09884a225453f41d5f80da93d45ae2d883d
yamashita-kenngo <yamashita-kenngo@outlook.jp>
2024-05-03 12:45:48 +0900

    fix: test

--- backend/src/index.test.ts
+++ backend/src/index.test.ts
@@ -3,6 +3,6 @@ import app from './index';
 describe('Test the root path', () => {
   test('It should response the GET method', async () => {
     const response = await app.request('http://localhost')
-    expect(response.status).toBe(200);
+    await expect(response.status).toBe(200);
   });
 });
