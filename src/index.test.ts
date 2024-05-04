f5252e0b448695be677f1451aa6a52c790de78f9
yamashita-kenngo <yamashita-kenngo@outlook.jp>
2024-05-04 09:28:09 +0900

    chore: fix test

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,5 +4,9 @@ describe('Test the root path', () => {
   test('It should response the GET method', async () => {
     const response = await app.request('http://localhost')
     expect(response.status).toBe(200);
+    response.json().then((data) => {
+      expect(data).toEqual({ message: 'Hello World!'});
+    }
+    );
   });
 });
