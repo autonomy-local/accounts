db718e47d477547209eeb81827bec170eabada14
Kengo Yamashita <yamashita-kenngo@outlook.jp>
2024-05-04 17:58:54 +0900

    chore: fix test (#12)
    * chore: fix test
    
    * refactor: use await

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,5 +4,8 @@ describe('Test the root path', () => {
   test('It should response the GET method', async () => {
     const response = await app.request('http://localhost')
     expect(response.status).toBe(200);
+    const data = await response.json();
+    expect(data).toEqual({ message: 'Hello World!' }
+    );
   });
 });
