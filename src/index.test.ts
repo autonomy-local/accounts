add29130438726f3c17925307d95cf101f0e510f
yamashita-kenngo <yamashita-kenngo@outlook.jp>
2024-05-04 17:57:05 +0900

    refactor: use await

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,9 +4,8 @@ describe('Test the root path', () => {
   test('It should response the GET method', async () => {
     const response = await app.request('http://localhost')
     expect(response.status).toBe(200);
-    response.json().then((data) => {
-      expect(data).toEqual({ message: 'Hello World!'});
-    }
+    const data = await response.json();
+    expect(data).toEqual({ message: 'Hello World!' }
     );
   });
 });
