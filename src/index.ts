6d14ee02b8134daded8b4b08bffdb7c31f99ff31
yamashita-kenngo <yamashita-kenngo@outlook.jp>
2024-05-10 17:23:12 +0900

    chore: add swagger ui

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { serve } from '@hono/node-server'
 import { OpenAPIHono } from '@hono/zod-openapi'
+import { swaggerUI } from '@hono/swagger-ui'
 import { route } from './router'
 
 const app = new OpenAPIHono()
@@ -8,13 +9,15 @@ app.openapi(route, (c) => {
   return c.json({ message: 'Hello World!' })
 })
 
-app.doc('/doc', {
+app.doc('/specification', {
   openapi: '3.0.0',
   info: {
-    title: 'My API',
+    title: 'Accounts API',
     version: '1.0.0'
   },
-})
+}).get('/doc', swaggerUI({
+  url: '/specification'
+}))
 
 const port = parseInt(process.env.PORT!) || 3000
 console.log(`Server is running on port ${port}`)
